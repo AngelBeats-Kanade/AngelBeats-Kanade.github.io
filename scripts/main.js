57da96b80103eb024bfcda21cef8133ed9999d71
AngelBeats <angelbeats177@gmail.com>
2020-01-04 19:19:06 +0800

    first demo

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,12 @@
-document.querySelector('img').onclick = function() {
-  alert('切换图片！如果没有切换请再点一次！');
-  if (document.querySelector('img').getAttribute('src') === 'images/1.jpg') {
-    document.querySelector('img').setAttribute('src', 'images/2.png');
-  } else {
-    document.querySelector('img').setAttribute('src', 'images/1.jpg');
+var mybutton = document.querySelector('button');
+
+mybutton.onclick = function() {
+  if (username.value == '')
+    alert('请输入用户名！');
+  else if (password.value == '')
+    alert('请输入密码！');
+  else {
+    alert('登录成功！');
+    window.location.href = 'test.html';
   }
-};
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-function setUserName() {
-  let myName = prompt('请输入你的名字');
-  if (!myName || myName === null) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Welcome!' + myName;
-  }
-};
-if (!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedname = localStorage.getItem('name');
-  myHeading.textContent = 'Welcome!' + storedname;
-}
-myButton.onclick = function() {
-  setUserName();
 }
