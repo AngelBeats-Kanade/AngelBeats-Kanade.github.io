014df1b6c0d807756bedc136c7634f903615ca4b
AngelBeats <angelbeats177@gmail.com>
2020-01-04 21:31:14 +0800

    0.01

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ mybutton.onclick = function() {
   else if (password.value == '')
     alert('请输入密码！');
   else {
-    alert('登录成功！');
-    window.location.href = 'test.html';
+    alert('登录成功！若未进入请再登录一次试试！');
+    top.location.href = 'test.html';
   }
 }
