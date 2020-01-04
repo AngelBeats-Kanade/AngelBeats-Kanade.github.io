bf9c3ce414280efbdaefc463af2fba4d1fa6c442
AngelBeats <angelbeats177@gmail.com>
2020-01-04 21:59:53 +0800

    0.01

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,8 @@ mybutton.onclick = function() {
   else if (password.value == '')
     alert('请输入密码！');
   else {
+    alert('登录成功！');
     window.location.href = 'test.html';
     window.event.returnValue = false;
-    alert('登录成功！');
   }
 }
