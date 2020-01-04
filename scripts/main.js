bcac744a6dc5a717fd8ccc9191e62ef68b7d1d37
AngelBeats-Kanade <58588522+AngelBeats-Kanade@users.noreply.github.com>
2020-01-04 21:51:05 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ mybutton.onclick = function() {
     alert('请输入密码！');
   else {
     alert('登录成功！若未进入请再登录一次试试！');
-    top.location.href = 'test.html';
-    top.event.returnValue = false;
+    window.location.href = 'test.html';
+    window.event.returnValue = false;
   }
 }
